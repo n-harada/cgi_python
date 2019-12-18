f3fb1cd93cf6adc24da31a13ac8733e6c4b92c1c
原田眞 <nao0815jas@yahoo.co.jp>
2019-12-18 16:06:37 +0900

    commit

--- cgi-bin/inference.py
+++ cgi-bin/inference.py
@@ -3,8 +3,8 @@
 import subprocess
 
 
-python_code1 = 'face_recognition_on_coral/face_recording_with_training'
-python_code2 = 'face_recognition_on_coral/face_detection_recognition_sklearn.py'
+python_code1 = '../face_recognition_on_coral/face_recording_with_training'
+python_code2 = '../face_recognition_on_coral/face_detection_recognition_sklearn.py'
 
 
 print('Content-type: text/html; charset=UTF-8\r\n')
@@ -12,8 +12,7 @@ print("<link rel='stylesheet' href='../style.css'>")
 print(
     "<h1 id='h1' class='learning'>学習中</h1><div class='loader'></div>", flush=True)
 
-cmd = "python3 cgi-bin/sleep.py"
-popen1 = subprocess.Popen(cmd.split())
+popen1 = subprocess.Popen(python_code1.split())
 popen1.wait()
 # print("<style type='text/css'>.learning,.loader{display:none}</style>", flush=True)
 print("<script>document.getElementById('h1').innerHTML='推論中'</script>", flush=True)
