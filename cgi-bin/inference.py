43bef926a067dd9eaf9be38d45e142b140f97461
原田眞 <nao0815jas@yahoo.co.jp>
2019-12-18 15:12:29 +0900

    commit

--- cgi-bin/inference.py
+++ cgi-bin/inference.py
@@ -1,21 +1,26 @@
 #!/usr/local/bin/python3
 # -*- coding:utf-8 -*-
+import subprocess
+
+
+python_code1 = 'face_recognition_on_coral/face_recording_with_training'
+python_code2 = 'face_recognition_on_coral/face_detection_recognition_sklearn.py'
 
 
 print('Content-type: text/html; charset=UTF-8\r\n')
 print("<link rel='stylesheet' href='../style.css'>")
-# print("<h1>学習中...</h1><div class='loader'>Loading...</div>", flush=True)
 print(
-    "<h1 class='learning'>学習中...</h1><div class='loader'></div>", flush=True)
+    "<h1 id='h1' class='learning'>学習中</h1><div class='loader'></div>", flush=True)
 
-import subprocess
-cmd = "python cgi-bin/sleep.py"
-# subprocess.run(cmd.split() ,check=True)
+cmd = "python3 cgi-bin/sleep.py"
 popen1 = subprocess.Popen(cmd.split())
 popen1.wait()
-print("<style type='text/css'>.learning,.loader{display:none}</style>", flush=True)
-print("<h1>学習終了</h1>", flush=True)
-cmd = "python cgi-bin/sleep.py"
+# print("<style type='text/css'>.learning,.loader{display:none}</style>", flush=True)
+print("<script>document.getElementById('h1').innerHTML='推論中'</script>", flush=True)
+# print("<h1>学習終了</h1>", flush=True)
+
+
+cmd = "python3 cgi-bin/sleep.py"
 popen2 = subprocess.Popen(cmd.split())
 popen2.wait()
-# print('<p>Hello, World!</p>', flush=True)
+
--- cgiserver.py
+++ cgiserver.py
@@ -4,9 +4,9 @@
 
 import http.server
 
-# server_address = ("", 8001)
-# handler_class = http.server.CGIHTTPRequestHandler #1 ハンドラを設定
-# server = http.server.HTTPServer(server_address, handler_class)
-# server.serve_forever()
+server_address = ("", 8001)
+handler_class = http.server.CGIHTTPRequestHandler #1 ハンドラを設定
+server = http.server.HTTPServer(server_address, handler_class)
+server.serve_forever()
 
-http.server.test(HandlerClass=http.server.CGIHTTPRequestHandler)
+# http.server.test(HandlerClass=http.server.CGIHTTPRequestHandler)
