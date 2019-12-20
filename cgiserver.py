b33fa7f1ef3388c4e45f828cc2242605d91c85cb
原田眞 <nao0815jas@yahoo.co.jp>
2019-12-20 12:41:14 +0900

    commit

--- cgiserver.py
+++ cgiserver.py
@@ -3,10 +3,13 @@
 
 
 import http.server
+import webbrowser
 
 server_address = ("", 8001)
 handler_class = http.server.CGIHTTPRequestHandler #1 ハンドラを設定
 server = http.server.HTTPServer(server_address, handler_class)
+webbrowser.open("http://127.0.0.1:8001")
 server.serve_forever()
+
 
 # http.server.test(HandlerClass=http.server.CGIHTTPRequestHandler)
